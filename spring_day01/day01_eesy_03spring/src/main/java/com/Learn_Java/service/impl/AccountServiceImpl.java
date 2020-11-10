b0020fa283245d282296d66400eb1c7c2a8d82aa
Jonekaka <973579614@qq.com>
2020-11-10 16:09:28 +0800

    spring基础第一阶段，解耦

---
+++ spring_day01/day01_eesy_03spring/src/main/java/com/Learn_Java/service/impl/AccountServiceImpl.java
@@ -0,0 +1,20 @@
+package com.Learn_Java.service.impl;
+
+import com.Learn_Java.dao.IAccountDao;
+import com.Learn_Java.service.IAccountService;
+
+/**
+ * 账户的业务层实现类
+ */
+public class AccountServiceImpl implements IAccountService {
+
+    private IAccountDao accountDao ;
+
+    public AccountServiceImpl(){
+        System.out.println("对象创建了");
+    }
+
+    public void  saveAccount(){
+        accountDao.saveAccount();
+    }
+}
